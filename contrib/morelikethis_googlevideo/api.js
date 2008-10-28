d15db20d5a4fb629854cfb7d1d6d9871e734bc0e
emackn <emackn>
2008-10-28 17:42:05 +0000

    Adding new google video module

---
+++ contrib/morelikethis_googlevideo/api.js
@@ -0,0 +1,3 @@
+ var source= ('http://www.google.com/uds/api?file=uds.js&v=1.0&source=uds-vbw');
+document.write ("<scr"+"ipt type='text/javascript' src='"+source);
+document.write ("'><\/scr"+"ipt>");
---
+++ contrib/morelikethis_googlevideo/gsvideobar.js
@@ -0,0 +1,3 @@
+ var source= ('http://www.google.com/uds/solutions/videobar/gsvideobar.js?mode=new');
+document.write ("<scr"+"ipt type='text/javascript' src='"+source);
+document.write ("'><\/scr"+"ipt>");
