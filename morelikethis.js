caf467de1af2881cf97841f6e1a8e938b6f4dd92
febbraro <febbraro>
2008-12-19 22:15:17 +0000

    added JS to manage the node edit form functionality

--- morelikethis.js
+++ morelikethis.js
@@ -1,11 +1,11 @@
-// $Id: morelikethis.js,v 1.1 2008/10/03 16:36:58 febbraro Exp $
+// $Id: morelikethis.js,v 1.1.2.1 2008/12/19 22:15:17 febbraro Exp $
 
 /**
  * Add the UI functionality for the term selction.
  */
 Drupal.behaviors.moreLikeThis = function(context) {
-	var list = $('#edit-morelikethis-taxonomy-terms', context);
-	list.click(function() {
+	// Bind to the Taxonomy Term list
+	$('#edit-morelikethis-taxonomy-terms', context).click(function() {
 		var terms = $('#edit-morelikethis-terms');
 
 		termVal = jQuery.trim(terms.val());
@@ -21,5 +21,27 @@ Drupal.behaviors.moreLikeThis = function(context) {
 		
 		terms.val(termVal + keyword);
 	});
+	
+	// Setup the Calais checkbox
+	if ($("#edit-morelikethis-use-calais-defaults").size() && $("#edit-morelikethis-use-calais-defaults").attr("checked")) {
+    // Hide its description and the taxonomy selector
+    $("#edit-morelikethis-terms").attr("disabled","disabled");
+    $("#edit-morelikethis-terms-wrapper > div.description").hide(0);
+    $("#edit-morelikethis-taxonomy-terms-wrapper").hide(0);
+  }
+  $("#edit-morelikethis-use-calais-defaults").click(function() {
+    if ($("#edit-morelikethis-use-calais-defaults").attr("checked")) {
+			// Use Calais is clicked. Disable/hide fields
+      $("#edit-morelikethis-terms").attr("disabled","disabled");
+      $("#edit-morelikethis-terms-wrapper > div.description").slideUp('fast');
+	    $("#edit-morelikethis-taxonomy-terms-wrapper").slideUp('fast');
+    }
+    else { // Unchecked show it all
+      $("#edit-morelikethis-terms").removeAttr("disabled");
+      $("#edit-morelikethis-terms-wrapper > div.description").slideDown('fast');
+	    $("#edit-morelikethis-taxonomy-terms-wrapper").slideDown('fast');
+    }
+  });
+  
 };
 
